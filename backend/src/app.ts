bf1210be7ad4c979500d9850eb8515f579a12cbc
AlexandreTonin <xandetonin@gmail.com>
2024-10-06 21:51:22 -0300

    Backend node.js project started

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log('Server is running on http://localhost:' + process.env.SERVER_PORT);
+});
